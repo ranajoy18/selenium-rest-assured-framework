cecda74963b40e0590fd4adfd003da7d6d90328d
Ranajoy Das <rdas19999@gmail.com>
2026-03-29 21:27:23 +0530

    configReader in place

---
+++ src/main/java/common/ConfigReader.java
@@ -0,0 +1,32 @@
+package common;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static final Properties property=new Properties();
+
+    static{
+        try(InputStream in=ConfigReader.class.getClassLoader().getResourceAsStream("config.properties")){
+            property.load(in);
+        }
+         catch (IOException e) {
+            throw new RuntimeException("config.properties not found",e);
+        }
+    }
+
+    public static String getBrowser(){
+        return property.getProperty("browser","chrome");
+    }
+    
+    public static String getHeadless(){
+        return property.getProperty("headless","false");
+    }
+
+    public static String getGridUrl(){
+        return property.getProperty("grid.url", "to-do");
+    }
+
+}
