59903feace25fc3e5c92dcf753539f779d73360e
Ranajoy Das <rdas19999@gmail.com>
2026-04-07 22:30:49 +0530

    convention fixed

---
+++ src/test/java/testRunners/LoginTest.java
@@ -0,0 +1,17 @@
+package testRunners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features/Login.feature",
+        glue = "stepDefinitions",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/LoginReport.html"
+        },
+        monochrome = true,
+        tags = "@login"
+)
+public class LoginTest extends AbstractTestNGCucumberTests {
+}
